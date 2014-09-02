6f250aaf99deb32c66da3e64ecfc68cff0ceeb2c
kimbos <kimberg88@gmail.com>
2014-09-02 19:44:43 +0200

    updated setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
 ln -s ~/opt/linux-configs/.vimrc ~/.vimrc
 ln -s ~/opt/linux-configs/.xmonad/ ~/.xmonad
+ln -s ~/opt/linux-configs/.xmobarrc ~/.xmobarrc
+ln -s ~/opt/linux-configs/.xinitrc ~/.xinitrc
+ln -s ~/opt/linux-configs/.Xresources ~/.Xresources
