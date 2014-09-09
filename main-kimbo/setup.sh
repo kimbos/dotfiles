6827627754ce4828b2001b2753bea04f76b492d5
kimbos <kimberg88@gmail.com>
2014-09-09 14:46:50 +0200

    Update setup.sh

--- main-kimbo/setup.sh
+++ main-kimbo/setup.sh
@@ -1,8 +1,10 @@
 # /bin/sh
 
-ln -s ~/opt/linux-configs/main-kimbo/.bashrc ~/.bashrc
-ln -s ~/opt/linux-configs/main-kimbo/.vimrc ~/.vimrc
-ln -s ~/opt/linux-configs/main-kimbo/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
-ln -s ~/opt/linux-configs/main-kimbo/.xmobarrc ~/.xmobarrc
-ln -s ~/opt/linux-configs/main-kimbo/.xinitrc ~/.xinitrc
-ln -s ~/opt/linux-configs/main-kimbo/.Xresources ~/.Xresources
+ln -s ~/opt/dotfiles/main-kimbo/.bashrc ~/.bashrc
+ln -s ~/opt/dotfiles/main-kimbo/.vimrc ~/.vimrc
+ln -s ~/opt/dotfiles/main-kimbo/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+ln -s ~/opt/dotfiles/main-kimbo/.xmobarrc ~/.xmobarrc
+ln -s ~/opt/dotfiles/main-kimbo/.xinitrc ~/.xinitrc
+ln -s ~/opt/dotfiles/main-kimbo/.Xresources ~/.Xresources
+ln -s ~/opt/dotfiles/main-kimbo/.weechat/weechat.conf ~/.weechat/weechat.conf
+ln -s ~/opt/dotfiles/main-kimbo/.weechat/buffers.conf ~/.weechat/buffers.conf
