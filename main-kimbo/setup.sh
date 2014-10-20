5e23bf801e49d383556664ed9c5a344bf675611d
kimbo <kimbo@deb-kimbo-main>
2014-10-20 22:43:41 +0200

    div

--- main-kimbo/setup.sh
+++ main-kimbo/setup.sh
@@ -8,4 +8,5 @@ ln -s ~/opt/dotfiles/main-kimbo/.xinitrc ~/.xinitrc
 ln -s ~/opt/dotfiles/main-kimbo/.Xresources ~/.Xresources
 ln -s ~/opt/dotfiles/main-kimbo/.weechat/weechat.conf ~/.weechat/weechat.conf
 ln -s ~/opt/dotfiles/main-kimbo/.weechat/buffers.conf ~/.weechat/buffers.conf
+ln -s ~/opt/dotfiles/main-kimbo/.weechat/irc.conf ~/.weechat/irc.conf
 ln -s ~/opt/dotfiles/main-kimbo/.asoundrc ~/.asoundrc
