308e95060855b5ab85b6e3d86a8dbc73a2386192
kimbo <kimbo@deb-kim10452>
2015-06-02 10:28:53 +0200

    sleep a bit before starting compton

--- main-kimbo/setup.sh
+++ main-kimbo/setup.sh
@@ -3,7 +3,7 @@
 ln -s ~/opt/dotfiles/main-kimbo/.Xresources ~/.Xresources
 ln -s ~/opt/dotfiles/main-kimbo/.bashrc ~/.bashrc
 ln -s ~/opt/dotfiles/main-kimbo/.xinitrc ~/.xinitrc
-ln -s ~/opt/dotfiles/main-kimbo/.config/compton.config ~/.config/compton.config
+ln -s ~/opt/dotfiles/main-kimbo/.config/compton.conf ~/.config/compton.conf
 
 ln -s ~/opt/dotfiles/main-kimbo/.vimrc ~/.vimrc
 ln -s ~/opt/dotfiles/main-kimbo/.i3 ~/.i3
