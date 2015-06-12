c517b17d3dd34624d1fd41d368cbdbb9f7afe8bc
kimbo <kimbo@deb-kim10452>
2015-06-12 14:51:44 +0200

    switched to i3-gaps

--- scripts/rdp-vcenter.sh
+++ scripts/rdp-vcenter.sh
@@ -5,9 +5,9 @@ User="mgk\admkb"
 
 
 ResX=$(echo "scale=0;($(xrandr | grep " primary " |  awk '{print $4}' | awk -F '+' '{print $1}' | awk -F 'x' '{print $1}'))" | bc)
-ResY=$(echo "scale=0;($(xrandr | grep " primary " |  awk '{print $4}' | awk -F '+' '{print $1}' | awk -F 'x' '{print $2}')-40)" | bc)
-Size=${ResX}x${ResY}
+ResY=$(echo "scale=0;($(xrandr | grep " primary " |  awk '{print $4}' | awk -F '+' '{print $1}' | awk -F 'x' '{print $2}')-23)" | bc)
 
+Size=${ResX}x${ResY}
 echo ${Size}
 
 rdesktop -k no -u ${User} ${Server} -r clipboard:PRIMARYCLIPBOARD -K -r disk:down=$HOME/Downloads -g ${Size}
